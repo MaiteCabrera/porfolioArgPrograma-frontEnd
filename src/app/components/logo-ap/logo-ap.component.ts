e1325ddb36006134634c862790d0cdff7826c43a
MaiteCabrera <maitecabrera11@gmail.com>
2022-09-27 23:34:02 -0300

    sigo sin poder iniciar sesion :c

--- src/app/components/logo-ap/logo-ap.component.ts
+++ src/app/components/logo-ap/logo-ap.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-logo-ap',
@@ -8,10 +9,24 @@ import { Router } from '@angular/router';
 })
 export class LogoAPComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  isLogged = false;
+
+  constructor(private router:Router, private tokenService:TokenService) { }
 
   ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged=true;
+    }else{
+      this.isLogged=false;
+    }
   }
+
+  onLogOut():void{
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+
+
   login(){
     this.router.navigate(['/login'])
   }
