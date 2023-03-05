40fa80454636473d8f171b521e2da1a22ec3bd2f
MaiteCabrera <maitecabrera11@gmail.com>
2023-03-05 15:38:51 -0300

    Error {headers: Hr, status: 0, statusText: 'Unknown Error ...

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -9,7 +9,7 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  URL = 'http://backendmaitecabrera.onrender.com/personas/';
+  URL = 'http://backendmaitecabrera.onrender.com/persona/';
   //URL = 'http://localhost:8080/personas/';
 
   constructor(private httpClient: HttpClient) { }
